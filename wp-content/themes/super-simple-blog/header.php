77cd756f4a32a032cf802ff4ec9187f85cd4b2db
Travis Turner <roboenigma@gmail.com>
2018-01-22 22:31:12 -0500

    Add some responsive CSS

--- wp-content/themes/super-simple-blog/header.php
+++ wp-content/themes/super-simple-blog/header.php
@@ -2,12 +2,13 @@
 <html>
 	<head>
 		<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 		<title><?php echo get_bloginfo( 'name' ); ?></title>
 		<link href="https://fonts.googleapis.com/css?family=Josefin+Sans:100,400" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css?family=Nunito+Sans" rel="stylesheet">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-		<link rel="stylesheet" href="<?php echo get_bloginfo('template_directory'); ?>/blog.css">
+		<link rel="stylesheet" href="<?php echo get_bloginfo('template_directory'); ?>/blog.css?ver=<?php echo rand(111,999)?>">
     <?php wp_head();?>
 	</head>
 	<body>
